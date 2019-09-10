aa00271b9efe07c9a0ba9d4c44345098645e720b
Hunter Chang <hunter@madwiremedia.com>
2019-09-10 17:05:08 -0600

    update menu

--- src/pages/fried-rice-lo-mein.js
+++ src/pages/fried-rice-lo-mein.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import LayoutInner from '../components/layoutInner'
 
 const FriedRiceIndex = () => (
@@ -9,40 +8,51 @@ const FriedRiceIndex = () => (
     <table className="table table-menu">
       <tbody>
         <tr>
+          <td></td>
+          <td className="menu-price">Lunch</td>
+          <td className="menu-price">Dinner</td>
+        </tr>
+        <tr>
           <td>
             <p className="menu-title">Chicken Fried Rice</p>
           </td>
-          <td className="menu-price">$9.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$10.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Pork Fried Rice</p>
           </td>
-          <td className="menu-price">$9.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$10.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Beef Fried Rice</p>
           </td>
-          <td className="menu-price">$9.45</td>
+          <td className="menu-price">$10.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Vegetable Fried Rice</p>
           </td>
-          <td className="menu-price">$9.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$10.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Shrimp Fried Rice</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$10.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Combination Fried Rice</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price"></td>
+          <td className="menu-price">$13.25</td>
         </tr>
         <tr>
           <td>
@@ -50,49 +60,64 @@ const FriedRiceIndex = () => (
               Volcano Fried Rice<strong className="text-red">*</strong>
             </p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price"></td>
+          <td className="menu-price">$13.25</td>
         </tr>
-
+      </tbody>
+    </table>
+    <table className="table table-menu">
+      <tbody>
         <tr>
-          <td colspan="2">
+          <td colspan="3">
             <h2>Lo Mein</h2>
           </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td className="menu-price">Lunch</td>
+          <td className="menu-price">Dinner</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Chicken Lo Mein</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Pork Lo Mein</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Beef Lo Mein</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$10.25</td>
+          <td className="menu-price">$12.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Vegetable Lo Mein</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Shrimp Lo Mein</p>
           </td>
-          <td className="menu-price">$11.45</td>
+          <td className="menu-price">$10.25</td>
+          <td className="menu-price">$12.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Combination Lo Mein</p>
           </td>
-          <td className="menu-price">$11.45</td>
+          <td className="menu-price"></td>
+          <td className="menu-price">$14.25</td>
         </tr>
       </tbody>
     </table>
--- src/pages/pork-chicken.js
+++ src/pages/pork-chicken.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import LayoutInner from '../components/layoutInner'
 
 const PorkChickenIndex = () => (
@@ -9,10 +8,16 @@ const PorkChickenIndex = () => (
     <table className="table table-menu">
       <tbody>
         <tr>
+          <td></td>
+          <td className="menu-price">Lunch</td>
+          <td className="menu-price">Dinner</td>
+        </tr>
+        <tr>
           <td>
             <p className="menu-title">Sweet &amp; Sour Pork</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
@@ -20,13 +25,15 @@ const PorkChickenIndex = () => (
               Twice Cooked Pork<strong className="text-red">*</strong>
             </p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price"></td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Pork with Vegetable</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
@@ -34,61 +41,78 @@ const PorkChickenIndex = () => (
               Pork with Garlic Sauce<strong className="text-red">*</strong>
             </p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Mu Shu Pork</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price"></td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Pork Chow Mein</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Pork with Mushrooms</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Pork Egg Foo Young</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price"></td>
+          <td className="menu-price">$12.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Thai Peanut Pork</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
-
+      </tbody>
+    </table>
+    <table className="table table-menu">
+      <tbody>
         <tr>
-          <td colspan="2">
+          <td colspan="3">
             <h2>Chicken</h2>
           </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td className="menu-price">Lunch</td>
+          <td className="menu-price">Dinner</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Sweet &amp; Sour Chicken</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Chicken with Broccoli</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Chicken with Cashew Nuts</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
@@ -96,19 +120,22 @@ const PorkChickenIndex = () => (
               Kung Pao Chicken<strong className="text-red">*</strong>
             </p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Moo Goo Gai Pan</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Lemon Chicken</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price"></td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
@@ -116,13 +143,15 @@ const PorkChickenIndex = () => (
               Chicken with Garlic Sauce<strong className="text-red">*</strong>
             </p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Chicken with Vegetables</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
@@ -130,31 +159,72 @@ const PorkChickenIndex = () => (
               Curry Chicken<strong className="text-red">*</strong>
             </p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Chicken Chow Mein</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Mu Shu Chicken</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price"></td>
+          <td className="menu-price">$12.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Chicken Egg Foo Young</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price"></td>
+          <td className="menu-price">$12.25</td>
         </tr>
         <tr>
           <td>
             <p className="menu-title">Thai Peanut Chicken</p>
           </td>
-          <td className="menu-price">$10.45</td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
+        </tr>
+        <tr>
+          <td>
+            <p className="menu-title">
+              Szechuan Chicken<strong className="text-red">*</strong>
+            </p>
+          </td>
+          <td className="menu-price">$9.25</td>
+          <td className="menu-price">$11.25</td>
+        </tr>
+        <tr>
+          <td>
+            <p className="menu-title">
+              Sesame Chicken<strong className="text-red">*</strong>
+            </p>
+          </td>
+          <td className="menu-price">$10.25</td>
+          <td className="menu-price">$13.25</td>
+        </tr>
+        <tr>
+          <td>
+            <p className="menu-title">
+              General Tso's Chicken<strong className="text-red">*</strong>
+            </p>
+          </td>
+          <td className="menu-price">$10.25</td>
+          <td className="menu-price">$13.25</td>
+        </tr>
+        <tr>
+          <td>
+            <p className="menu-title">
+              Tangerine Chicken<strong className="text-red">*</strong>
+            </p>
+          </td>
+          <td className="menu-price">$10.25</td>
+          <td className="menu-price">$13.25</td>
         </tr>
       </tbody>
     </table>
